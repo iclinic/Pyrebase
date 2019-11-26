9eecd1e2ee867ff9784af3dafe6b64bc6a3c6c41
rfunix <rafinha.unix@gmail.com>
2019-11-26 16:58:23 -0300

    Remove commented code

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'requests>=2.13.0',
-#        'gcloud==0.17.0',
-#        'oauth2client==3.0.0',
         'requests_toolbelt==0.7.0',
         'python_jwt==2.0.1',
         'pycryptodome==3.4.3'
