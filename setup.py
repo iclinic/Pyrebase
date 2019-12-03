9210c60f13b09a2ae7835ce6143c1e664894ae94
rfunix <rafinha.unix@gmail.com>
2019-12-03 18:37:40 -0300

    Fix package name and version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='iclinic-Pyrebase',
+    name='iclinic-pyrebase',
     version='1.0.0',
     url='https://github.com/thisbejim/Pyrebase',
     description='A simple python wrapper for the Firebase API',
